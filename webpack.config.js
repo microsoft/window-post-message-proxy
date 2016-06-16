9e570c80b75560ca0d583dc3021a6fd6b4526413
Matt Mazzola <mattm@microsoft.com>
2016-06-16 13:09:59 -0700

    Add minifaction to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
-module.exports = {  
-  entry: './src/windowPostMessageProxy.ts',
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'windowPostMessageProxy': './src/windowPostMessageProxy.ts',
+    'windowPostMessageProxy.min': './src/windowPostMessageProxy.ts'
+  },
   output: {
     path: __dirname + "/dist",
-    filename: 'windowPostMessageProxy.js',
+    filename: '[name].js',
     library: 'window-post-message-proxy',
     libraryTarget: 'umd'
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true
+    })
+  ],
   devtool: 'source-map',
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
