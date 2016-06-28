1db46f3eea4a9fd15a74535a7f74e2586be628ac
Matt Mazzola <mattm@microsoft.com>
2016-06-28 14:44:47 -0700

    Change webpack config to output library matching package name

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var package = require('./package.json');
+
 module.exports = {
   entry: {
     'windowPostMessageProxy': './src/windowPostMessageProxy.ts'
@@ -5,7 +7,7 @@ module.exports = {
   output: {
     path: __dirname + "/dist",
     filename: '[name].js',
-    library: 'window-post-message-proxy',
+    library: package.name,
     libraryTarget: 'umd'
   },
   devtool: 'source-map',
