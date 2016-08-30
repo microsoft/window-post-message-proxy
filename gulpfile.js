70140b3209d23be92619c3b7d7579f7e4be535fd
Matt Mazzola <mattm@microsoft.com>
2016-08-30 15:53:21 -0700

    Include es6-promise .d.ts in sources for gulp docs task.

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('ghpages', 'Deploy documentation to gh-pages', ['nojekyll'], function 
 
 gulp.task("docs", 'Compile documentation from src code', function () {
   return gulp
-    .src(["src/**/*.ts"])
+    .src(["typings/globals/es6-promise/index.d.ts", "src/**/*.ts"])
     .pipe(typedoc({
       mode: 'modules',
       includeDeclarations: true,
